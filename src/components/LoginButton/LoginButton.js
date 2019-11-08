4acd97f1421de8db3e19439efc0afdb8e221de8d
Hasan Kemal Demirci <ha_rap@hotmail.com>
2019-11-08 17:35:15 +0300

    Updates LoginButton component

--- src/components/LoginButton/LoginButton.js
+++ src/components/LoginButton/LoginButton.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { authEndpoint, clientId, redirectUri, scopes } from "../../config";
 
 // Styles
 import "./LoginButton.scss";
@@ -8,17 +7,30 @@ import "./LoginButton.scss";
 import SpotifyLogo from "../../img/spotify-icon.png";
 
 const LoginButton = () => {
-  return (
-    <a
-      className="login-button"
-      href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-        "%20"
-      )}&response_type=token&show_dialog=true`}
-    >
-      <img src={SpotifyLogo} alt="Login to Spotify" className="login-button__icon" />
-      Login to Spotify
-    </a>
-  );
+  const authURL = process.env.REACT_APP_AUTH_URL;
+  const clientId = process.env.REACT_APP_CLIENT_ID;
+  const redirectUri = process.env.REACT_APP_REDIRECT_URI;
+  const scopes = process.env.REACT_APP_SCOPES;
+
+  const Link = () => {
+    const appScopes = JSON.parse(scopes);
+    const url = `${authURL}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${appScopes.join(
+      "%20"
+    )}&response_type=token&show_dialog=true`;
+
+    return (
+      <a href={url} className="login-button">
+        <img
+          src={SpotifyLogo}
+          alt="Login to Spotify"
+          className="login-button__icon"
+        />
+        Login to Spotify
+      </a>
+    );
+  };
+
+  return <Link />;
 };
 
 export default LoginButton;
