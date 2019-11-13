782a44c4ff17efbd678a709e3f8821714ea1f736
Hasan Kemal Demirci <ha_rap@hotmail.com>
2019-11-13 20:02:38 +0300

    Edits login page's layout and styles

--- src/routes/Login/Login.js
+++ src/routes/Login/Login.js
@@ -3,10 +3,20 @@ import React from 'react';
 // Styles
 import './Login.scss';
 
+// Components
+import LoginButton from '../../components/LoginButton/LoginButton';
+
+// Images
+import Logo from '../../img/logo.png';
+
 const Login = () => (
   <div className="login">
     <div className="container">
-      <h1>Login</h1>
+      <div className="login__content">
+        <h1>My Playlists</h1>
+        <img src={Logo} className="logo" alt="My Playlists" />
+        <LoginButton />
+      </div>
     </div>
   </div>
 );
